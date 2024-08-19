06b8bee16b384a9dda2cd7265ee8ac81b8346019
antoniox20 <antoniotaboada777@gmail.com>
2024-08-18 20:58:15 -0400

    Limpiar caché y eliminar archivos grandes

--- release-builds/MyApp-win32-x64/resources/app/src/__test__/test-port.js
+++
@@ -1,8 +0,0 @@
-const portInUse = require('port-in-use');
-
-// Verifica si el puerto 3000 está en uso
-portInUse(3000).then(inUse => {
-    console.log(`El puerto 3000 ${inUse ? 'está en uso' : 'no está en uso'}`);
-}).catch(err => {
-    console.error(`Error: ${err}`);
-});
